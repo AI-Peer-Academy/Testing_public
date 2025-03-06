e323e4019ac78ddec1fd0dce6c042f27dacbf8da
ManojKumarM75 <114361477+ManojKumarM75@users.noreply.github.com>
2025-03-06 20:31:27 +0400

    Update Test_pdf_cloud_20250306.py

--- Test_pdf_cloud_20250306.py
+++ Test_pdf_cloud_20250306.py
@@ -4,19 +4,30 @@ from PIL import Image
 import io
 import os
 
-import streamlit as st
-
-# Google Drive PDF URL (converted)
+# Google Drive PDF URL
 file_id = "1CMU4xK3u_wAGD0Ev_YsV-shC88ujXi83"
 pdf_url = f"https://drive.google.com/file/d/{file_id}/preview"
 
-# Page number input
-page_number = st.number_input("Go to page:", min_value=1, value=3, step=1)
+# Set default page number to 3
+if "page_number" not in st.session_state:
+    st.session_state.page_number = 3  # Default to page 3
 
-# Display PDF with page number support
-st.components.v1.iframe(f"{pdf_url}#page={page_number}", width=800, height=600)
+# Allow user to change the page
+page_number = st.number_input("Go to page:", min_value=1, value=st.session_state.page_number, step=1)
 
+# Embed PDF using HTML & JavaScript for automatic page navigation
+pdf_embed_code = f"""
+    <script>
+        function loadPDF() {{
+            var iframe = document.getElementById("pdf_frame");
+            iframe.src = "{pdf_url}#page={page_number}";
+        }}
+        window.onload = loadPDF;
+    </script>
+    <iframe id="pdf_frame" src="{pdf_url}#page={page_number}" width="800" height="600"></iframe>
+"""
 
+st.components.v1.html(pdf_embed_code, height=650)
 
 def open_pdfViewer_as_image(pdf_file, page_number=2):
     try:
