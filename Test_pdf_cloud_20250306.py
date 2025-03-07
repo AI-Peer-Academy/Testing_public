bf5b5a3de9451df18a07a330b5abb12d5542e393
ManojKumarM75 <114361477+ManojKumarM75@users.noreply.github.com>
2025-03-07 11:07:33 +0400

    Update Test_pdf_cloud_20250306.py

--- Test_pdf_cloud_20250306.py
+++ Test_pdf_cloud_20250306.py
@@ -1,7 +1,7 @@
 # Function to display the PDF using PDF.js
 def display_pdf_with_pdfjs(pdf_base64, initial_page=1):
     pdf_js_code = f"""
-    <html>
+    <html>
     <head>
         <head>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.10.377/pdf.min.js"></script>
